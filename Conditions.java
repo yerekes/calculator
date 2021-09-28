5f94e2098b826154b1b7ae616e10bef9213cc949
yerekes <smagulovyerzat@gmail.com>
2021-09-28 10:54:47 +0300

    Перепроверка

--- Conditions.java
+++ Conditions.java
@@ -2,31 +2,34 @@ import java.util.Scanner;
 
 public class Conditions{
     static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
         int j = getVozrastInt();
         float rost = getRostInt();
     }
+
     public static int getVozrastInt() {
         System.out.println(" Choose your age:");
         int j;
-    if(scanner.hasNextInt() ){
+        if(scanner.hasNextInt() ){
         j = scanner.nextInt();
-        if(j>20){
+            if(j > 20){
             System.out.println("Starik");
-        } else {
+            } else {
             System.out.println("Molodoy brariska");
             }
         }
     return 0;
     }    
+    
      public static float getRostInt() {
         System.out.println(" Choose your height:");
         float rost;
-    if(scanner.hasNextFloat() ){
+        if(scanner.hasNextFloat() ){
         rost = scanner.nextFloat();
-        if(rost>1.80f){
+            if(rost > 1.80f){
             System.out.println("Jiraf");    
-        } else {
+            } else {
             System.out.println("korotyshka");
             }     
         }
