d7ce50efedfaafaa64f7b59dc77393fcf05d0bb9
yerekes <smagulovyerzat@gmail.com>
2021-09-28 09:53:02 +0300

    читабелен

--- Conditions.java
+++ Conditions.java
@@ -5,36 +5,33 @@ public class Conditions{
     public static void main(String[] args) {
         int j = getVozrastInt();
         float rost = getRostInt();
-      
-     }
-     public static int getVozrastInt() {
-            System.out.println(" Choose your age:");
-            int j;
-     if(scanner.hasNextInt() ){
-         j = scanner.nextInt();
-          if(j>20){
-             System.out.println("Starik");
-    
-        } else{
-             System.out.println("Molodoy brariska");
-             }
     }
+     public static int getVozrastInt() {
+        System.out.println(" Choose your age:");
+        int j;
+     if(scanner.hasNextInt() ){
+        j = scanner.nextInt();
+        if(j>20){
+        System.out.println("Starik");
+        } else {
+        System.out.println("Molodoy brariska");
+        }
+        }
     return 0;
-}    
+    }    
      public static float getRostInt() {
-            System.out.println(" Choose your height:");
-            float rost;
+        System.out.println(" Choose your height:");
+        float rost;
     if(scanner.hasNextFloat() ){
-         rost = scanner.nextFloat();
-         if(rost>1.80f){
-            System.out.println("Jiraf");
-            
-        } else{
-            System.out.println("korotyshka");
-         }     
-      }
+        rost = scanner.nextFloat();
+        if(rost>1.80f){
+        System.out.println("Jiraf");    
+        } else {
+        System.out.println("korotyshka");
+        }     
+        }
         return 0;
-}  
+    }  
 }
 
     
