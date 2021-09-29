7ff369d8657fdd3ebb6dcffa07cabb16125eb402
yerekes <smagulovyerzat@gmail.com>
2021-09-29 12:23:06 +0300

    syerzst

--- Conditions.java
+++ Conditions.java
@@ -1,11 +1,13 @@
 import java.util.Scanner;
 
+
 public class Conditions{
     static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
         int j = getVozrastInt();
         float rost = getRostInt();
+        String name = getNameStr();
     }
 
     public static int getVozrastInt() {
@@ -35,6 +37,19 @@ public class Conditions{
         }
         return 0;
     }  
+    public static String getNameStr(){
+        System.out.println("Choose your name:");
+        String name;
+        if(scanner.hasNext()){
+            name = scanner.nextLine();
+            System.out.println("Your name lol:"+ name);
+        }else {
+            System.out.println("Smotri 4to ty pishesh!!!!");
+            scanner.next();
+            name = getNameStr();
+        }
+        return name;
+    }
 }
 
     
